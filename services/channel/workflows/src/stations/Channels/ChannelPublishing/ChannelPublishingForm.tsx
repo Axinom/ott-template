9e6e5c8c6429abad8e4a6d2e7c2e6d223d80f1d7
Isuru <92033440+axisuru@users.noreply.github.com>
2024-11-22 18:57:33 +0530

    feat: added quickedit to publishing stations (#388)

---
+++ services/channel/workflows/src/stations/Channels/ChannelPublishing/ChannelPublishingForm.tsx
@@ -0,0 +1,58 @@
+import { FormStation, MessageBar } from '@axinom/mosaic-ui';
+import React from 'react';
+import { client } from '../../../apolloClient';
+import {
+  PublishValidationStatus,
+  useValidateChannelQuery,
+} from '../../../generated/graphql';
+import { ValidationResultsTable } from '../../../util/Publishing/ValidationResultsTable';
+import { useActions } from './ChannelPublishing.actions';
+import classes from './ChannelPublishing.module.scss';
+import { FormData } from './ChannelPublishing.types';
+
+interface ChannelPublishingFormProps {
+  channelId: string;
+}
+
+export const ChannelPublishingForm: React.FC<ChannelPublishingFormProps> = ({
+  channelId,
+}) => {
+  const { loading, data, error } = useValidateChannelQuery({
+    client,
+    variables: { id: channelId },
+    fetchPolicy: 'no-cache',
+  });
+
+  const { actions } = useActions(
+    channelId,
+    data?.validateChannel?.validationStatus,
+    data?.validateChannel?.publishHash,
+  );
+
+  return (
+    <FormStation<FormData>
+      defaultTitle="Publishing"
+      initialData={{
+        loading,
+        data: data?.validateChannel,
+        error: error?.message,
+      }}
+      actions={actions}
+      saveData={() => {
+        // We don't need to save the data, since this station only displays the validation status.
+      }}
+      edgeToEdgeContent={true}
+    >
+      {data?.validateChannel?.validationStatus ===
+        PublishValidationStatus.Errors && (
+        <MessageBar
+          type="info"
+          title="Channel cannot be published. Please check validation errors and fix related data accordingly."
+        />
+      )}
+      <div className={classes.content}>
+        <ValidationResultsTable />
+      </div>
+    </FormStation>
+  );
+};
